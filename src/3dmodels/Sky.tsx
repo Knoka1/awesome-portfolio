d247e10480cd16a3b342be3840acb2165b62ec43
Knoka1 <knoka1@hotmail.com>
2024-01-31 17:21:15 -0300

    feature: Adding plane animation

--- src/3dmodels/Sky.tsx
+++ src/3dmodels/Sky.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import { useGLTF } from "@react-three/drei";
-
+import { ModelsObjectDetails } from "../models/ModelsObjectDetailsModel";
 import skyScene from "../assets/3d/sky.glb";
-const Sky = () => {
+import { useFrame } from "@react-three/fiber";
+import { HTMLMesh } from "three/examples/jsm/Addons.js";
+
+const Sky: React.FC<ModelsObjectDetails> = ({ isRotating }) => {
   const sky = useGLTF(skyScene);
+  const skyRef = useRef();
+
+  useFrame((_, delta) => {
+    if (isRotating) {
+      skyRef.current.rotation.y += 0.15 * delta;
+    }
+  });
   return (
-    <mesh>
+    <mesh ref={skyRef}>
       <primitive object={sky.scene} />
     </mesh>
   );
