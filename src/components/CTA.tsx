f6d9a05a668e2c536d7edc32283498c73eac9e07
Knoka1 <knoka1@hotmail.com>
2024-02-26 23:09:49 -0300

    feature: CTA.tsx translation added

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 const CTA = () => {
+  const { t, ready } = useTranslation();
+  if (!ready) return "loading translations...";
+
   return (
     <section className="cta">
       <p className="cta-text">
-        Have a project in mind? <br className="sm:block hidden" />
-        Let's build something together!
+        {t("components.CTA.introduction")}
+        <br className="sm:block hidden" /> {t("components.CTA.finalization")}
       </p>
       <Link to="/contact" className="btn">
-        Contact
+        {t("components.CTA.contact")}
       </Link>
     </section>
   );
