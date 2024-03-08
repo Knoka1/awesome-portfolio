a8b04111a3672258b682296b752e2c69350f43dd
Knoka1 <knoka1@hotmail.com>
2024-03-08 17:44:44 -0300

    feature: Added translations for stages in HomeInfo.tsx

--- src/components/HomeInfo.tsx
+++ src/components/HomeInfo.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 // @ts-ignore
 import { arrow } from "../assets/icons";
+import { useTranslation } from "react-i18next";
 interface Props {
   text: string;
   link: string;
@@ -16,37 +17,41 @@ const InfoBox: React.FC<Props> = ({ text, link, btnText }) => (
     </Link>
   </div>
 );
-const renderContent: any = {
-  1: (
-    <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
-      Hi, I am <span className="font-semibold">Alec</span>
-      <br />A Software Engineer from Brazil
-    </h1>
-  ),
-  2: (
-    <InfoBox
-      text="Worked with many companies and picked up many skills"
-      link="/projects"
-      btnText="Visit my portfolio"
-    />
-  ),
-  3: (
-    <InfoBox
-      text="Worked with many companies and picked up many skills"
-      link="/Contact"
-      btnText="Learn More"
-    />
-  ),
-  4: (
-    <InfoBox
-      text="Worked with many companies and picked up many skills"
-      link="/about"
-      btnText="Learn More"
-    />
-  ),
-};
 
 const HomeInfo = ({ currentStage }) => {
+  const { t } = useTranslation();
+  const renderContent: any = {
+    1: (
+      <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
+        {t("components.homeinfo.1.introduction")}{" "}
+        <span className="font-semibold">Alec</span>
+        <br />
+        {t("components.homeinfo.1.subtitle")}
+      </h1>
+    ),
+    2: (
+      <InfoBox
+        text={`${t("components.homeinfo.2.introduction")}`}
+        link="/projects"
+        btnText="Visit my portfolio"
+      />
+    ),
+    3: (
+      <InfoBox
+        text={`${t("components.homeinfo.3.introduction")}`}
+        link="/Contact"
+        btnText="Learn More"
+      />
+    ),
+    4: (
+      <InfoBox
+        text={`${t("components.homeinfo.4.introduction")}`}
+        link="/about"
+        btnText="Learn More"
+      />
+    ),
+  };
+
   return renderContent[currentStage] || null;
 };
 
