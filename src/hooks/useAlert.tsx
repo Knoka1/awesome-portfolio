d88008f9fab0ad656bd48bc7798b4f36fc5b132e
Knoka1 <knoka1@hotmail.com>
2024-03-20 00:04:26 -0300

    fix: Cleanup of unused assets

--- src/hooks/useAlert.tsx
+++ src/hooks/useAlert.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 
+export interface IAlert {
+  text: string;
+  type: string;
+}
 const useAlert = () => {
   const [alert, setAlert] = useState({ show: false, text: "", type: "danger" });
-  const showAlert = ({ text, type = "danger" }) =>
+  const showAlert = ({ text, type = "danger" }: IAlert) =>
     setAlert({ show: true, text, type });
   const hideAlert = () => setAlert({ show: false, text: "", type: "danger" });
   return { alert, showAlert, hideAlert };
